e5e119f181c0f2b4b2e10ff56cf0c799f3963b1a
cleamorales <cleaamorales@gmail.com>
2026-04-09 12:41:15 -0500

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-})
+const pool = mysql.createPool(process.env.DATABASE_URL)
 
 export default pool.promise()
