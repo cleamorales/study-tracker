2388c4cce09c2232a18a59995aa4cb4961fdc12c
cleamorales <cleaamorales@gmail.com>
2026-04-08 21:28:07 -0500

    fix vercel routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/study-behavior-tracker/', // your repo name
+  base: '/study-tracker/', // your repo name
   plugins: [react()],
 })
