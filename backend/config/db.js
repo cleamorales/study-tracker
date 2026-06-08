2d77953fab404e673e2156743395a7cbf9335bf8
cleamorales <cleaamorales@gmail.com>
2026-06-07 20:15:15 -0500

    add SSL support for Aiven MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,15 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const pool = mysql.createPool(process.env.DATABASE_URL)
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 export default pool.promise()
